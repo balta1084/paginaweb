2bdf20d0f42e870b0ee0abde75a782c349ee1bf8
balta1084 <baltamendez55@gmail.com>
2026-05-05 23:36:40 -0300

    prueba agregando java

---
+++ Eje.java
@@ -0,0 +1,12 @@
+public class Eje {
+
+    public static void main(String[] args) {
+        int age = 22;
+        if (age >= 18) {
+            System.out.println("Allowed");
+        } else {
+            System.out.println("Not allowed");            
+        }
+
+    }
+}
